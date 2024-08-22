9b7818ff4772c309a79017902f36902201897c29
barroscgo <gabriel_cb_oliveira@estudante.sesisenai.org.br>
2024-08-21 21:07:59 -0300

    .

--- OT11/apiRest/index.js
+++ OT11/apiRest/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import colecaoUf from './dados/dados.js'
+import colecaoUf from './dados.js'
 
 const app= express();
 
 app.get('/ufs', (req, res) =>{
     res.json(colecaoUf)
 });
+
+app.get('/ufs/teste', (req, res) =>{
+    res.send({"teste":"teste"})
+})
 
 app.listen(8080, () =>{
     console.log('Servidor iniciado na porta 8080')
